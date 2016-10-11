4823af1ce95bd0a1858ad30f11d5c16dbea97b19
Adjoen <ahmad_junaidi22@yahoo.com>
2016-10-11 17:17:35 +0700

    upload pertama

---
+++ index.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Adjoen GitHub</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="bootstrap-3.3.7-dist/css/bootstrap.min.css">
+
+  <script src="jquery-3.1.1\dist/jquery.min.js"></script>
+  <script src="bootstrap-3.3.7-dist/js/bootstrap.min.js"></script>
+   <link rel="stylesheet" href="style.css">   
+</head>
+<body>
+
+<nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">Adjoen</a>
+    </div>
+    <div>
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="#">Home</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
+  
+<div class="container">
+  <div class="jumbotron">
+    <h1>Ahmad Junaidi Site</h1>    
+
+    <p>Project Euler ID</p>
+    <p>"Adjoen"</p>     
+
+  </div>
+
+  <div class="row">
+    <div class="header">
+      <h2 style="color:black;">View In</h2>
+    </div>
+    <div class="content">
+      <div class="col-md-4">
+        <strong><a href="https://github.com/Adjoen/hello-world/zipball/master">ZIP File</a></strong>
+      </div>
+      <div class="col-md-4"> 
+        <strong><a href="https://github.com/Adjoen/hello-world/tarball/master">TAR Ball</a></strong>
+      </div>
+      <div class="col-md-4"> 
+        <strong><a href="https://github.com/Adjoen/hello-world">GitHub</a></strong>
+      </div>
+      <div class="clearfix visible-lg"></div>
+    </div>
+    </div>
+  </div>
+</div>
+
+
+ <div class="container">
+  <div class="berita">
+    <div class="judul">
+    <h1>Lorem Ipsum</h1>
+    </div>
+    <div class="Tanggal">
+    <p class="date">10-10-2016</p>
+    </div>
+    <div class="isi">
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    </div>
+  </div>
+ </div>
+
+<footer>
+  <a href="#">Site Map</a>
+  <a href="#">Privacy Policy</a>
+  <a href="#">@2016</a>
+</footer>
+
+</body>
+
+<!-- Mirrored from www.w3schools.com/bootstrap/tryit.asp?filename=trybs_case_navbar&stacked=h by HTTrack Website Copier/3.x [XR&CO'2014], Fri, 23 Jan 2015 07:34:16 GMT -->
+</html>
